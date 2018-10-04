65004fe02b4b54ec0c43bfa848d54884ea7084e4
funcylambda <lambda@edwardhaigh.com>
2018-10-04 18:52:34 +0100

    Init

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.ML.Legacy;
+using Microsoft.ML.Legacy.Data;
+using Microsoft.ML.Legacy.Trainers;
+using Microsoft.ML.Legacy.Transforms;
+
+
+namespace ML_Clustering
+{
+    internal class Program
+    {
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "IrisClusteringModel.zip");
+
+        private static async Task Main(string[] args)
+        {
+            PredictionModel<IrisData, ClusterPrediction> model = Train();
+            await model.WriteAsync(_modelPath);
+
+            var prediction = model.Predict(TestIrisData.Setosa);
+            Console.WriteLine($"Cluster: {prediction.PredictedClusterId}");
+            Console.WriteLine($"Distances: {string.Join(" ", prediction.Distances)}");
+
+            Console.ReadKey();
+        }
+
+        private static PredictionModel<IrisData, ClusterPrediction> Train()
+        {
+            var pipeline = new LearningPipeline();
+            pipeline.Add(new TextLoader(_dataPath).CreateFrom<IrisData>(separator: ','));
+
+            pipeline.Add(new ColumnConcatenator(
+                "Features",
+                "SepalLength",
+                "SepalWidth",
+                "PetalLength",
+                "PetalWidth"));
+
+            pipeline.Add(new KMeansPlusPlusClusterer() { K = 3 });
+
+            var model = pipeline.Train<IrisData, ClusterPrediction>();
+            return model;
+        }
+    }
+}
